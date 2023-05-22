a104cbc2213bf217e127b0435c5ccedccee9afab
Martí Gelabert Gómez <57955364+martigelabert@users.noreply.github.com>
2023-05-23 00:29:28 +0200

    finito

--- test3.py
+++ test3.py
@@ -1,5 +1,5 @@
 
-
+# watch https://www.youtube.com/watch?v=0yy83vTnx-4
 # TODO: https://github.com/pydicom/pydicom/blob/master/examples/image_processing/reslice.py
 # TODO: https://www.raddq.com/dicom-processing-segmentation-visualization-in-python/
 # DICOM loading and visualization
@@ -21,6 +21,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from PIL import Image
 import scipy
+import matplotlib
 
 def MIP_sagittal_plane(img_dcm: np.ndarray) -> np.ndarray:
     """ Compute the maximum intensity projection on the sagittal orientation. """
@@ -90,24 +91,66 @@ segmentation = []
 print(f'mask {ds}')
 for i in ds.pixel_array:
     segmentation.append(i)
+segmentation = np.array(segmentation)
+if False:
+    masked = []
+    for index,s in enumerate(slides3d):
+        rgb_image = plt.cm.gray(s)
+        alpha = 0.2
+        mask = segmentation[index]
+        masked_overlay = np.copy(rgb_image)
 
-masked = []
-for index,s in enumerate(slides3d):
-    rgb_image = plt.cm.gray(s)
-    alpha = 0.2
-    mask = segmentation[index]
-    masked_overlay = np.copy(rgb_image)
+        for i in range(len(masked_overlay)):
+            for j in range(masked_overlay.shape[1]):
+                if mask[i][j] == 1:
+                    masked_overlay[i][j][0]= int(100) # Red channel
 
-    for i in range(len(masked_overlay)):
-        for j in range(masked_overlay.shape[1]):
-            if mask[i][j] == 1:
-                masked_overlay[i][j][0]= int(255) # Red channel
-    #masked_overlay[:, :, mask]= 255
-    #masked_overlay[..., 1:3] *= alpha  # Green and blue channels
-    masked.append(Image.fromarray((masked_overlay * 255).astype(np.uint8)))
-    #masked.append(Image.fromarray(masked_overlay))
-    #plt.imshow(masked_overlay)
-    #plt.show()
+        #masked_overlay[:, :, mask]= 255
+        #masked_overlay[..., 1:3] *= alpha  # Green and blue channels
+        #masked.append(Image.fromarray((masked_overlay * 255).astype(np.uint8)))
+        #masked.append(Image.fromarray(masked_overlay))
+
+        #masked.append((masked_overlay * 255).astype(np.uint8))
+
+        #plt.imshow(masked_overlay)
+        #plt.show()
+
+        masked = np.array(masked)
+###################################################################33
+# Create projections varying the angle of rotation
+#   Configure visualization colormap
+#img_dcm = masked#slides3d
+slides3d[segmentation[0:89]] = 255
+img_dcm = slides3d
+img_min = np.amin(img_dcm)
+img_max = np.amax(img_dcm)
+cm = matplotlib.colormaps['bone']
+fig, ax = plt.subplots()
+#   Configure directory to save results
+os.makedirs('results/MIP/', exist_ok=True)
+#   Create projections
+n = 6
+projections = []
+for idx, alpha in enumerate(np.linspace(0, 360 * (n - 1) / n, num=n)):
+    rotated_img = rotate_on_axial_plane(img_dcm, alpha)
+    projection = MIP_sagittal_plane(rotated_img)
+    plt.imshow(projection, cmap=cm, vmin=img_min, vmax=img_max, aspect=pixel_len_mm[0] / pixel_len_mm[1])
+    plt.savefig(f'results/MIP/Projection_{idx}.png')  # Save animation
+    projections.append(projection)  # Save for later animation
+# Save and visualize animation
+animation_data = [
+    [plt.imshow(img, animated=True, cmap=cm, vmin=img_min, vmax=img_max, aspect=pixel_len_mm[0] / pixel_len_mm[1])]
+    for img in projections
+]
+anim = animation.ArtistAnimation(fig, animation_data,
+                                 interval=250, blit=True)
+anim.save('results/MIP/Animation.gif')  # Save animation
+plt.show()  # Show animation
+#########################################################################################################################3
+#for i in slides3d:
+#    plt.imshow(MIP_sagittal_plane(slides3d), cmap=matplotlib.colormaps['bone'], aspect=pixel_len_mm[0] / pixel_len_mm[1])
+#    plt.show()
+
 
 if False:
     masked[0].save('masked1.gif', format='GIF',
